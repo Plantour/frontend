4b661fa7393565cabd9c62606c91fb921825ba10
ybmin1 <yebin.min1@gmail.com>
2024-08-16 23:27:42 +0900

    updating landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,7 +2,20 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 // 각 페이지 컴포넌트
-const Page1 = () => <div>내 주변 식물들의 위치를 확인해보세요!</div>;
+const Page1 = () => (
+  <>
+    <div>
+      <UserMarker>
+        <UserMarkerRound></UserMarkerRound>
+      </UserMarker>
+    </div>
+    <Title>내 주변 식물들의 위치를 확인해보세요!</Title>
+    <Desc>
+      다른 사용자가 찾은 식물들의 위치를 볼 수 있고, 마커를 클릭하면 식물 사진과
+      설명을 볼 수 있어요.
+    </Desc>
+  </>
+);
 const Page2 = () => <div>미션을 수행해보세요!</div>;
 const Page3 = () => <div>식물 노트를 작성해보세요!</div>;
 
@@ -11,24 +24,75 @@ const pages = [Page1, Page2, Page3];
 const LandingPageLayout = styled.div`
   width: 100%;
   height: 100%;
-  background-color: lightblue;
+  background-color: rgb(250, 254, 255);
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
 `;
 
-const Button = styled.button`
+const Title = styled.h1`
+  font-size: 1rem;
+`;
+
+const Desc = styled.p`
+  font-size: 0.875rem;
+`;
+
+const UserMarker = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: red;
+  border-radius: 50% 50% 50% 50% / 0% 50% 50% 50%;
+  transform: rotate(-135deg);
+  position: relative;
+`;
+
+const UserMarkerRound = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: #971a1a;
+  position: absolute;
+  bottom: 15px;
+  right: 15px;
+`;
+
+const ButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5px;
+`;
+
+const NextButton = styled.button`
+  width: 90%;
   padding: 10px 20px;
   margin: 0 10px;
-  background-color: #007bff;
-  color: white;
+  background-color: rgba(102, 221, 221, 0.7);
+  color: black;
   border: none;
   border-radius: 5px;
   cursor: pointer;
 
   &:hover {
-    background-color: #0056b3;
+    background-color: #66dddd;
+  }
+`;
+
+const SkipButton = styled.button`
+  width: 90%;
+  padding: 10px 20px;
+  margin: 0 10px;
+  background-color: rgba(255, 255, 255, 0.1);
+  color: black;
+  border: 1px solid lightgrey;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.1);
   }
 `;
 
@@ -53,16 +117,16 @@ const LandingPage = ({ onComplete }) => {
   return (
     <LandingPageLayout>
       <CurrentPageComponent />
-      <div>
+      <ButtonContainer>
         {currentPage < pages.length - 1 ? (
-          <Button onClick={nextPage}>Next</Button>
+          <NextButton onClick={nextPage}>Next</NextButton>
         ) : (
-          <Button onClick={skipIntro}>Get Started</Button>
+          <SkipButton onClick={skipIntro}>Get Started</SkipButton>
         )}
         {currentPage < pages.length - 1 && (
-          <Button onClick={skipIntro}>Skip</Button>
+          <SkipButton onClick={skipIntro}>Skip</SkipButton>
         )}
-      </div>
+      </ButtonContainer>
     </LandingPageLayout>
   );
 };
