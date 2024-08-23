a215449007742d4ebee9d49a1870fe74114be6fe
ybmin1 <yebin.min1@gmail.com>
2024-08-24 01:23:59 +0900

    update MapModal FormattedData function

--- src/components/Map/MapModal.jsx
+++ src/components/Map/MapModal.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { IoCloseCircleOutline } from "react-icons/io5";
+import { useLanguage } from "../../helpers/languageUtils";
 
 const ModalOverlay = styled.div`
   position: fixed;
@@ -65,53 +66,71 @@ const Content = styled.p`
 `;
 
 const MapModal = ({ showModal, onClose, markerData }) => {
+  const { language } = useLanguage();
   if (!showModal) {
     return null;
   }
 
+  // 언어에 따라 날짜 형식 변경
+  const showFormattedDate = (dateStr) => {
+    const dateObj = new Date(dateStr);
+
+    if (language === "en") {
+      return dateObj.toLocaleDateString("en-GB", {
+        day: "numeric",
+        month: "short",
+        year: "numeric",
+      });
+    } else {
+      return `${dateObj.getFullYear()}년 ${
+        dateObj.getMonth() + 1
+      }월 ${dateObj.getDate()}일`;
+    }
+  };
+
   //시간데이터에서 년도, 월, 일 추출하기
   // 주어진 날짜 문자열
 
-  const showDDMMYY = (dateStr) => {
-    // Date 객체로 변환
-    const dateObj = new Date(dateStr);
+  // const showDDMMYY = (dateStr) => {
+  //   // Date 객체로 변환
+  //   const dateObj = new Date(dateStr);
 
-    // 영국식 날짜 형식 (8th Aug 2024)
-    const day = dateObj.getDate();
-    const monthNames = [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec",
-    ];
-    const month = monthNames[dateObj.getMonth()];
-    const year = dateObj.getFullYear();
+  //   // 영국식 날짜 형식 (8th Aug 2024)
+  //   const day = dateObj.getDate();
+  //   const monthNames = [
+  //     "Jan",
+  //     "Feb",
+  //     "Mar",
+  //     "Apr",
+  //     "May",
+  //     "Jun",
+  //     "Jul",
+  //     "Aug",
+  //     "Sep",
+  //     "Oct",
+  //     "Nov",
+  //     "Dec",
+  //   ];
+  //   const month = monthNames[dateObj.getMonth()];
+  //   const year = dateObj.getFullYear();
 
-    // 서수 접미사 계산
-    const getOrdinalSuffix = (day) => {
-      if (day > 3 && day < 21) return "th"; // 4th - 20th
-      switch (day % 10) {
-        case 1:
-          return "st"; // 1st, 21st, 31st
-        case 2:
-          return "nd"; // 2nd, 22nd
-        case 3:
-          return "rd"; // 3rd, 23rd
-        default:
-          return "th"; // 4th, 5th, ...
-      }
-    };
+  //   // 서수 접미사 계산
+  //   const getOrdinalSuffix = (day) => {
+  //     if (day > 3 && day < 21) return "th"; // 4th - 20th
+  //     switch (day % 10) {
+  //       case 1:
+  //         return "st"; // 1st, 21st, 31st
+  //       case 2:
+  //         return "nd"; // 2nd, 22nd
+  //       case 3:
+  //         return "rd"; // 3rd, 23rd
+  //       default:
+  //         return "th"; // 4th, 5th, ...
+  //     }
+  //   };
 
-    return `${day}${getOrdinalSuffix(day)} ${month} ${year}`;
-  };
+  //   return `${day}${getOrdinalSuffix(day)} ${month} ${year}`;
+  // };
 
   //imageUrl에 https://가 없으면 앞에 붙여주기
   const imageUrl = markerData.imageUrl;
@@ -125,7 +144,7 @@ const MapModal = ({ showModal, onClose, markerData }) => {
         <SectionTopContainer onClick={onClose}>
           <SectionTopLeft>
             <div>{markerData.nickname}</div>
-            <div>{showDDMMYY(markerData.completedAt)}</div>
+            <div>{showFormattedDate(markerData.completedAt)}</div>
           </SectionTopLeft>
           <StyledIoCloseCircleOutline />
         </SectionTopContainer>
